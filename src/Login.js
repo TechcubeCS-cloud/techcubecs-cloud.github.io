de48e80875725e75a0517e16825146338a3926df
srinathan <kskandu@live.com>
2023-02-02 11:25:40 +0000

    name change

--- src/Login.js
+++ src/Login.js
@@ -38,7 +38,7 @@ function Login() {
         <div>
             <div className='auth-form-container'>
                 <form className='login-form'>
-                    <h1>Business Access</h1>
+                    <h1>Tech Business Access</h1>
                     <label>Customer ID</label>
                     <input value={cusId} onChange={(e) => setcusId(e.target.value)} type="custId" placeholder="Customer ID" />
                     <label>Business ID</label>
