4c931e36a5a7528c9f18a93f0b2aff2137deb025
srinathan <kskandu@live.com>
2023-02-02 12:07:16 +0000

    method

--- src/Login.js
+++ src/Login.js
@@ -18,10 +18,11 @@ function Login() {
     }
 
     const doLogin = async (cusId, busId) => {
-        const response = await fetch(variables.API_URL_LOGIN + "cusId=" + cusId + "&busId=" + busId, +
-        <meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests'/>+{
+        const response = await fetch(variables.API_URL_LOGIN + "cusId=" + cusId + "&busId=" + busId,+
+        <meta httpEquiv='Content-Security-Policy' content='upgrade-insecure-requests'/>+ {
             'mode': 'cors',
-            
+            'referrerPolicy':"unsafe-url",
+            'method':'GET'
             
         }).then(res => res.json())
             .then(data => {
@@ -38,7 +39,7 @@ function Login() {
         <div>
             <div className='auth-form-container'>
                 <form className='login-form'>
-                    <h1>Business Access</h1>
+                    <h1>Techcube Business Access</h1>
                     <label>Customer ID</label>
                     <input value={cusId} onChange={(e) => setcusId(e.target.value)} type="custId" placeholder="Customer ID" />
                     <label>Business ID</label>
